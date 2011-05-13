ceae8dea8a78ee2cb2b656f15eb2f64f46ff5891
Petr Vanek <petr@yarpen.cz>
2011-05-13 12:45:55 +0000

    copyrights

--- razorqt-session/src/razormodman.cpp
+++ razorqt-session/src/razormodman.cpp
@@ -1,3 +1,29 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ *
+ * Razor - a lightweight, Qt based, desktop toolset
+ * https://sourceforge.net/projects/razor-qt/
+ *
+ * Copyright: 2010-2011 Razor team
+ * Authors:
+ *   Petr Vanek <petr@scribus.info>
+ *   Christopher Regali
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation;  only version 2 of
+ * the License is valid for this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
 #include "razormodman.h"
 #include <razorqt/readsettings.h>
 #include <razorqt/xdgautostart.h>
