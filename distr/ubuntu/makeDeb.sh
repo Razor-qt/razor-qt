438aa9b8eb9c1e6d1e24bd16d89a05a5af45d5eb
SokoloffA <sokoloff.a@gmail.com>
2011-05-27 18:40:30 +0000

    Deb package building script and files.

--- distr/ubuntu/makeDeb.sh
+++ distr/ubuntu/makeDeb.sh
@@ -6,6 +6,7 @@ KEY='29EE0D60'
 SVN='https://razor-qt.svn.sourceforge.net/svnroot/razor-qt/trunk'
 RELEASES="maverick natty"
 
+
 VER=''
 SVN_DIR=''
 TYPE='-S'
@@ -18,9 +19,9 @@ while [ $# -gt 0 ]; do
         shift 2
       ;;
 
-    --type)
-        TYPE=$2
-        shift 2
+    -b|--binary)
+        TYPE='-b'
+        shift
       ;;
 
     --)
@@ -40,41 +41,51 @@ done
 
 
 if [ "$error" ]; then
-    echo "Usage: makeDeb.sh [--ver VERSION] [--type TYPE]"
+    echo "Usage: makeDeb.sh [--ver VERSION] [--binary]"
     exit
 fi
 
 if [ -z "$VER" ]; then
     VER=`LANG=C svn info ${SVN} | grep '^Revision:' | awk '{print $2}'`
+    VER=378
     NAME=$NAME-svn
 fi
 
 echo "*******************************"
-echo " Dist: ${DIST}"
 echo " Name: ${NAME}"
 echo " Ver:  ${VER}"
-echo " RELEASES: ${RELEASES}"
+echo " Type: ${TYPE}" 
+echo " Releases: ${RELEASES}"
 echo "*******************************"
 
 DIR=${NAME}-${VER}
 svn export --force ${SVN} ${DIR}
 tar czf ${NAME}_${VER}.orig.tar.gz ${DIR}
 
-SRC_DEB_DIR=${SVN_DIR}/distr/ubuntu/debian
+DATE=`date -R`
+CUR_DIR=`pwd`
+for RELEASE in $RELEASES; do
+    echo "*******************************"
+    echo " Build $RELEASE"
+    echo "*******************************"
 
-#cp --force -R ${SRC_DEB_DIR} ${DIR}/debian
-#DATE=`date -R`
-#for f in `find ${SRC_DEB_DIR} -type f \;`; do
-#   sed -i \
-#       -e"s/%NAME%/${NAME}/g"  \
-#       -e"s/%VERSION%/${VER}/g" \
-#       -e"s/%DIST%/${DIST}/g" \
-#       -e"s/%DATE%/${DATE}/g" \
-#       ${SRC_DEB_DIR}/$f
-#done
+    cd $CUR_DIR
+    SRC_DEB_DIR=${DIR}/distr/ubuntu/debian
+    cp --force -R ${SRC_DEB_DIR} ${DIR}/ || exit 1
 
-#cd ${DIR} && dpkg-buildpackage -rfakeroot 
-#cd ${DIR} && debuild ${TYPE} -sa -k${KEY}
+    for f in `find ${DIR}/debian -type f `; do
+        sed -i \
+            -e"s/%NAME%/${NAME}/g"  \
+            -e"s/%VERSION%/${VER}/g" \
+            -e"s/%DIST%/${RELEASE}/g" \
+            -e"s/%DATE%/${DATE}/g" \
+            $f
+    done
+
+    cd ${DIR} && debuild ${TYPE} -rfakeroot -sa -k${KEY}
+done
+
+
 
 
 
