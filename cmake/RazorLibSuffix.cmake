60b06055f5779f67abe917ee440cac8e01d184a9
SokoloffA <sokoloff.a@gmail.com>
2011-05-31 15:11:18 +0000

    A new algorithm of the  LIB_SUFFIX autodetection.

--- cmake/RazorLibSuffix.cmake
+++ cmake/RazorLibSuffix.cmake
@@ -1,24 +1,24 @@
 # some system (rpm builds) setup LIB_SUFFIX for cmake. If there is no set, try to get it from system
 IF (NOT DEFINED LIB_SUFFIX)
-	MESSAGE(STATUS "")
-	MESSAGE(STATUS "LIB_SUFFIX variable is not defined. It will be autodetected now.")
-	MESSAGE(STATUS "You can set it manually with -DLIB_SUFFIX=<value> (64 for example)")
-	IF (NOT APPLE)
-		# check 64 bit
-		IF (CMAKE_SIZEOF_VOID_P EQUAL 4)
-			SET (LIB_SUFFIX "")
-		ELSE (CMAKE_SIZEOF_VOID_P EQUAL 4)
-			SET (LIB_SUFFIX "64")
-			MESSAGE(STATUS " LIB_SUFFIX is set to '${LIB_SUFFIX}'")
-		ENDIF (CMAKE_SIZEOF_VOID_P EQUAL 4)
-	ELSE (NOT APPLE)
-		SET (LIB_SUFFIX "")
-	ENDIF (NOT APPLE)
-	# BSD does not use lib64 for 64bit libs
-	IF (${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD" OR ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD")
-		message(STATUS "LIB_SUFFIX is disabled for *BSD: ${CMAKE_SYSTEM_NAME}")
-		SET (LIB_SUFFIX "")
-	ENDIF()
-	#
-	MESSAGE(STATUS "")
+    MESSAGE(STATUS "*********************************************************************")
+    MESSAGE(STATUS "LIB_SUFFIX variable is not defined. It will be autodetected now")
+    MESSAGE(STATUS "You can set it manually with -DLIB_SUFFIX=<value> (64 for example)")
+
+    # All 32bit system have empty lib suffix
+    if(CMAKE_SIZEOF_VOID_P EQUAL 8)
+        # If there is lib64 dir, set suffix to 64
+        if(IS_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib64)
+            set(LIB_SUFFIX 64)
+        elseif(IS_DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib)
+            set(LIB_SUFFIX "")
+        else()
+            message(WARNING "LIB_SUFFIX cannot be autodetected. No ${CMAKE_INSTALL_PREFIX}/lib neither ${CMAKE_INSTALL_PREFIX}/lib64 found.")
+            set(LIB_SUFFIX "")
+        endif()
+    else()
+        set(LIB_SUFFIX "")
+    endif()
+
+    message(STATUS "LIB_SUFFIX autodetected as '${LIB_SUFFIX}', libraries will be installed into ${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX}")
+    MESSAGE(STATUS "*********************************************************************")
 ENDIF (NOT DEFINED LIB_SUFFIX)
