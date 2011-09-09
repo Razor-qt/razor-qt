ce0814eeb7b715d7e77c927504df75e33a12f2af
Alexander Sokolov <sokoloff.a@gmail.com>
2011-09-09 13:45:38 +0400

    Test page

---
+++ install.php
@@ -0,0 +1,33 @@
+<h2>Get Razor-qt</h2>
+Get the latest stable version of Razor-qt for your operating system.<p>
+
+<div class=installInfo>
+<img src='imgs/distros/ubuntu.png' width=48 height=48>
+Ubuntu users can use repository at <a href="ehttps://launchpad.net/~razor-qt/+archive/ppa/">launchpad</a>.<br>See <a href=install/ubuntu.php>Install Ubuntu</a> page for instructions.
+</div>
+
+
+<div class=installInfo>
+<img src='imgs/distros/opensuse.png'>
+	SUSE users could use up-to-date packages from <a href="ehttp://download.opensuse.org/repositories/home://TI_Eugene://QtDesktop/">QtDesktop repository</a>.
+</div>
+
+<div class=installInfo>
+<img src='imgs/distros/fedora.png'>
+Fedora users could use up-to-date packages from <a href="ehttp://download.opensuse.org/repositories/home://TI_Eugene://QtDesktop/">QtDesktop repository</a>.
+</div>
+
+
+<div class=installInfo>
+<img src='imgs/distros/archlinux.png'>
+Arch Linux user can install it form <a href="https://aur.archlinux.org/packages.php?ID=49663">AUR</a>.
+</div>
+
+
+<div class=installInfo>
+<img src='imgs/distros/agilialinux.png'>
+Agilia Linux users can install it form <a href="http://packages.agilialinux.ru/show.php?id=15334">Userland</a> repository.
+</div>
+
+
+
