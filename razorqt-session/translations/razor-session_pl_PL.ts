d6dbca1b523e373e08aaa3ff7024f936f8dfd9ed
Maciej Płaza <plaza.maciej@gmail.com>
2011-10-03 19:45:56 +0200

    Plugins translations
    Signed-off-by: Maciej Płaza <plaza.maciej@gmail.com>

---
+++ razorqt-session/translations/razor-session_pl_PL.ts
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="37"/>
+        <source>Openbox - light-weight window manager</source>
+        <translation>Openbox - lekki menedżer okien</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="38"/>
+        <source>KWin - window manager of the KDE Software Compilation</source>
+        <translation>KWin - menedżer okien z KDE SC</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="39"/>
+        <source>Metacity - window manager of the GNOME desktop environment</source>
+        <translation>Metacity - menedżer okien z GNOME</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="40"/>
+        <source>Windowmaker - a classical lightweight window manager</source>
+        <translation>Windowmaker - klasyczny menedżer okien</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="41"/>
+        <source>Enlightenement 16</source>
+        <translation>Enlightenement 16</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="42"/>
+        <source>Fvwm2 - a classical lightweight window manager</source>
+        <translation>Fvwm2 - klasyczny menedżer okien</translation>
+    </message>
+</context>
+<context>
+    <name>WmSelectDialog</name>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="14"/>
+        <source>Welcome to Razor-qt</source>
+        <translation>Witamy w Razor-Qt</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="20"/>
+        <source>&lt;b&gt;Welcome to Razor-qt&lt;/b&gt;
+&lt;p&gt;
+Before starting to use the Razor-qt, you might want to select the Windows Manager:</source>
+        <translation>&lt;b&gt;Witamy w Razor-Qt&lt;/b&gt;
+&lt;p&gt;
+Przed rozpoczęciem pracy wybierz menedżer okien:</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="35"/>
+        <source>You can change the Window Manager later at any time via Razor Session Configurator.</source>
+        <translation>Możesz zmienić menedżer okien później w Konfiguracji Sesji Razor.</translation>
+    </message>
+</context>
+</TS>
