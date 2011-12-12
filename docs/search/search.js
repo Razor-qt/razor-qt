543621bfe11465a348b6db33f9e74dd6d90174e6
Alexander Sokolov <sokoloff.a@gmail.com>
2011-12-12 11:48:11 +0400

    Documentation update

---
+++ docs/search/search.js
@@ -0,0 +1,25 @@
+function SearchBox(name, resultsPath, inFrame, label)
+{
+  this.searchLabel = label;
+  this.DOMSearchField = function()
+  {  return document.getElementById("MSearchField");  }
+  this.DOMSearchBox = function()
+  {  return document.getElementById("MSearchBox");  }
+  this.OnSearchFieldFocus = function(isActive)
+  {
+    if (isActive)
+    {
+      this.DOMSearchBox().className = 'MSearchBoxActive';
+      var searchField = this.DOMSearchField();
+      if (searchField.value == this.searchLabel) 
+      {
+        searchField.value = '';
+      }
+    }
+    else
+    {
+      this.DOMSearchBox().className = 'MSearchBoxInactive';
+      this.DOMSearchField().value   = this.searchLabel;
+    }
+  }
+}
