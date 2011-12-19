58d22cb89208f134b4c7e2028fd2a5317074a1a4
Alexander Sokolov <sokoloff.a@gmail.com>
2011-12-19 16:25:12 +0400

    Typo in russian translation

--- libraries/razorqt/translations/librazorqt_ru_RU.ts
+++ libraries/razorqt/translations/librazorqt_ru_RU.ts
@@ -25,95 +25,113 @@
     </message>
 </context>
 <context>
+    <name>Battery</name>
+    <message>
+        <location filename="../librazor-upower/battery.cpp" line="68"/>
+        <source>%1 : Properties changed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../librazor-upower/battery.cpp" line="74"/>
+        <source>%1 : Low</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../librazor-upower/battery.cpp" line="84"/>
+        <source>%1 : Full</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>PowerManager</name>
     <message>
-        <location filename="../powermanager.cpp" line="91"/>
+        <location filename="../powermanager.cpp" line="94"/>
         <source>Hibernate</source>
         <translation>Спящий режим</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="95"/>
+        <location filename="../powermanager.cpp" line="101"/>
         <source>Suspend</source>
         <translation>Ждущий режим</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="99"/>
+        <location filename="../powermanager.cpp" line="108"/>
         <source>Reboot</source>
         <translation>Перезагрузка</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="103"/>
+        <location filename="../powermanager.cpp" line="115"/>
         <source>Shutdown</source>
         <translation>Выключить компьютер</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="107"/>
+        <location filename="../powermanager.cpp" line="120"/>
         <source>Logout</source>
         <translation>Завершить сеанс</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="119"/>
+        <location filename="../powermanager.cpp" line="132"/>
         <source>Razor Session Suspend</source>
         <translation>Ждущий режим</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="120"/>
+        <location filename="../powermanager.cpp" line="133"/>
         <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
         <translation>Перевести компьютер в ждущий режим?&lt;p&gt;Состояние системы будет потеряно при отключении питания.</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="131"/>
+        <location filename="../powermanager.cpp" line="144"/>
         <source>Razor Session Hibernate</source>
         <translation>Спящий режим</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="132"/>
+        <location filename="../powermanager.cpp" line="145"/>
         <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
         <translation>Перевести компьютер в спящий режим?&lt;p&gt;Состояние системы будет сохранено при отключении питания.</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="143"/>
+        <location filename="../powermanager.cpp" line="156"/>
         <source>Razor Session Reboot</source>
         <translation>Перезагрузка</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="144"/>
+        <location filename="../powermanager.cpp" line="157"/>
         <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
         <translation>Перезагрузить компьютер? Все не сохраненные документы будут потеряны...</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="155"/>
+        <location filename="../powermanager.cpp" line="168"/>
         <source>Razor Session Shutdown</source>
         <translation>Выключение компьютера</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="156"/>
+        <location filename="../powermanager.cpp" line="169"/>
         <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
         <translation>Выключить компьютер? Все не сохраненные документы будут потеряны...</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="167"/>
+        <location filename="../powermanager.cpp" line="180"/>
         <source>Razor Session Logout</source>
         <translation>Завершение сеанса</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="168"/>
+        <location filename="../powermanager.cpp" line="181"/>
         <source>Do you want to really logout? All unsaved work will be lost...</source>
         <translation>Завершить сеанс?&lt;p&gt;Все не сохраненные документы будут потеряны...</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="180"/>
-        <location filename="../powermanager.cpp" line="185"/>
+        <location filename="../powermanager.cpp" line="193"/>
+        <location filename="../powermanager.cpp" line="198"/>
         <source>Razor Power Manager Error</source>
         <translation>Ошибка Razor Power Manager</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="180"/>
+        <location filename="../powermanager.cpp" line="193"/>
         <source>Hibernate failed.</source>
         <translation>Ошибка перехода в спящий режим.</translation>
     </message>
     <message>
-        <location filename="../powermanager.cpp" line="185"/>
+        <location filename="../powermanager.cpp" line="198"/>
         <source>Suspend failed.</source>
         <translation>Ошибка перехода в ждущий режим.</translation>
     </message>
@@ -121,12 +139,12 @@
 <context>
     <name>RazorShortcutButton</name>
     <message>
-        <location filename="../razorshortcutbutton.cpp" line="46"/>
+        <location filename="../razorshortcutbutton.cpp" line="48"/>
         <source>Clear</source>
         <translation>Очистить</translation>
     </message>
     <message>
-        <location filename="../razorshortcutbutton.cpp" line="158"/>
+        <location filename="../razorshortcutbutton.cpp" line="160"/>
         <source>None</source>
         <translation>Не задано</translation>
     </message>
@@ -134,53 +152,53 @@
 <context>
     <name>ScreenSaver</name>
     <message>
-        <location filename="../screensaver.cpp" line="47"/>
+        <location filename="../screensaver.cpp" line="48"/>
         <source>Lock Screen</source>
         <translation>Заблокировать экран</translation>
     </message>
     <message>
-        <location filename="../screensaver.cpp" line="66"/>
-        <location filename="../screensaver.cpp" line="74"/>
-        <location filename="../screensaver.cpp" line="82"/>
-        <location filename="../screensaver.cpp" line="94"/>
-        <location filename="../screensaver.cpp" line="102"/>
-        <location filename="../screensaver.cpp" line="110"/>
-        <location filename="../screensaver.cpp" line="119"/>
+        <location filename="../screensaver.cpp" line="67"/>
+        <location filename="../screensaver.cpp" line="75"/>
+        <location filename="../screensaver.cpp" line="83"/>
+        <location filename="../screensaver.cpp" line="95"/>
+        <location filename="../screensaver.cpp" line="103"/>
+        <location filename="../screensaver.cpp" line="111"/>
+        <location filename="../screensaver.cpp" line="120"/>
         <source>Screen Saver Activation Error</source>
         <translation>Ошибка запуска хранителя экрана</translation>
     </message>
     <message>
-        <location filename="../screensaver.cpp" line="67"/>
+        <location filename="../screensaver.cpp" line="68"/>
         <source>An error occurred starting screensaver. xdg-screensaver cannot be started due its crash.</source>
-        <translation>При запуске произошла ошибка. Не вышло запустить xdg-screenserver по причине его ошибки.</translation>
+        <translation>При запуске произошла ошибка. Не вышло запустить xdg-screensaver по причине его ошибки.</translation>
     </message>
     <message>
-        <location filename="../screensaver.cpp" line="75"/>
+        <location filename="../screensaver.cpp" line="76"/>
         <source>An error occurred starting screensaver. xdg-screensaver is not installed correctly.</source>
-        <translation>При запуске произошла ошибка. Xdg-screenserver не установлен по человечески.</translation>
+        <translation>При запуске произошла ошибка. Xdg-screensaver не установлен по человечески.</translation>
     </message>
     <message>
-        <location filename="../screensaver.cpp" line="83"/>
+        <location filename="../screensaver.cpp" line="84"/>
         <source>An error occurred starting screensaver. xdg-screensaver cannot be started.</source>
-        <translation>При запуске произошла ошибка. Xdg-screenserver не запущен.</translation>
+        <translation>При запуске произошла ошибка. Xdg-screensaver не запущен.</translation>
     </message>
     <message>
-        <location filename="../screensaver.cpp" line="95"/>
+        <location filename="../screensaver.cpp" line="96"/>
         <source>An error occurred starting screensaver. Syntax error in xdg-screensaver arguments.</source>
-        <translation>При запуске произошла ошибка. Не верные аргументы запуска xdg-screenserver.</translation>
+        <translation>При запуске произошла ошибка. Не верные аргументы запуска xdg-screensaver.</translation>
     </message>
     <message>
-        <location filename="../screensaver.cpp" line="103"/>
+        <location filename="../screensaver.cpp" line="104"/>
         <source>An error occurred starting screensaver. Ensure you have xscreensaver installed and running.</source>
-        <translation>При запуске произошла ошибка. Убедитесь, что xdg-screenserver установлен и запущен.</translation>
+        <translation>При запуске произошла ошибка. Убедитесь, что xdg-screensaver установлен и запущен.</translation>
     </message>
     <message>
-        <location filename="../screensaver.cpp" line="111"/>
+        <location filename="../screensaver.cpp" line="112"/>
         <source>An error occurred starting screensaver. Action &apos;activate&apos; failed. Ensure you have xscreensaver installed and running.</source>
-        <translation>При запуске произошла ошибка. Не могу активизировать его. Xdg-screenserver не установлен по человечески.</translation>
+        <translation>При запуске произошла ошибка. Не могу активизировать его. Xdg-screensaver не установлен по человечески.</translation>
     </message>
     <message>
-        <location filename="../screensaver.cpp" line="120"/>
+        <location filename="../screensaver.cpp" line="121"/>
         <source>An error occurred starting screensaver. Unkown error - undocumented return value from xdg-screensaver=%1.</source>
         <translation>При запуске произошла ошибка. Не известная ошибка - недокументрованый код возврата xdg-screensaver=%1.</translation>
     </message>
@@ -188,7 +206,7 @@
 <context>
     <name>about</name>
     <message>
-        <location filename="../aboutdlg.ui" line="14"/>
+        <location filename="../razoraboutdlg.ui" line="14"/>
         <source> About Razor</source>
         <translation>О программе</translation>
     </message>
