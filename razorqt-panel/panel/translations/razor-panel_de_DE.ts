1989b3e9b408c17024a30a19362da2e23c0f6224
Wubbbi <e.ashrafinia@gmail.com>
2011-12-27 02:33:54 +0100

    Initial german release, only a tiny part

---
+++ razorqt-panel/panel/translations/razor-panel_de_DE.ts
@@ -0,0 +1,143 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>ConfigPanelDialog</name>
+    <message>
+        <location filename="../configpaneldialog.ui" line="26"/>
+        <source>Configure panel</source>
+        <translation>Panel konfigurieren</translation>
+    </message>
+    <message>
+        <location filename="../configpaneldialog.ui" line="38"/>
+        <source>Height:</source>
+        <translation>Höhe:</translation>
+    </message>
+    <message>
+        <location filename="../configpaneldialog.ui" line="67"/>
+        <source>OK</source>
+        <translation>OK</translation>
+    </message>
+    <message>
+        <location filename="../configpaneldialog.ui" line="81"/>
+        <source>%</source>
+        <translation>%</translation>
+    </message>
+    <message>
+        <location filename="../configpaneldialog.ui" line="86"/>
+        <source>px</source>
+        <translation>px</translation>
+    </message>
+    <message>
+        <location filename="../configpaneldialog.ui" line="100"/>
+        <source>Width:</source>
+        <translation>Breite:</translation>
+    </message>
+    <message>
+        <location filename="../configpaneldialog.ui" line="130"/>
+        <source>Left</source>
+        <translation>Links</translation>
+    </message>
+    <message>
+        <location filename="../configpaneldialog.ui" line="135"/>
+        <source>Right</source>
+        <translation>Rechts</translation>
+    </message>
+    <message>
+        <location filename="../configpaneldialog.ui" line="140"/>
+        <source>Center</source>
+        <translation>Mitte</translation>
+    </message>
+    <message>
+        <location filename="../configpaneldialog.ui" line="154"/>
+        <source>Alignment</source>
+        <translation>Ausrichtung</translation>
+    </message>
+</context>
+<context>
+    <name>PositionAction</name>
+    <message>
+        <location filename="../razorpanel.cpp" line="544"/>
+        <source>Top of desktop</source>
+        <translation>Oben auf dem Desktop</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="545"/>
+        <source>Bottom of desktop</source>
+        <translation>Unten auf dem Desktop</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="546"/>
+        <source>Left of desktop</source>
+        <translation>Links auf dem Desktop</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="547"/>
+        <source>Right of desktop</source>
+        <translation>Rechts auf dem Desktop</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="554"/>
+        <source>Top of desktop %1</source>
+        <translation>Oben auf dem Desktop %1</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="555"/>
+        <source>Bottom of desktop %1</source>
+        <translation>Unten auf dem Desktop %1</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="556"/>
+        <source>Left of desktop %1</source>
+        <translation>Links auf dem Desktop %1</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="557"/>
+        <source>Right of desktop %1</source>
+        <translation>Rechts auf dem Desktop %1</translation>
+    </message>
+</context>
+<context>
+    <name>RazorPanelPrivate</name>
+    <message>
+        <location filename="../razorpanel.cpp" line="755"/>
+        <source>Panel</source>
+        <translation>Panel</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="766"/>
+        <source>Plugins</source>
+        <translation>Plugins</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="768"/>
+        <source>Add plugins ...</source>
+        <translation>Plugins hinzufügen ...</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="777"/>
+        <source>Move plugin</source>
+        <translation>Plugin verschieben</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="783"/>
+        <source>Configure plugin</source>
+        <translation>Plugin konfigurieren</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="790"/>
+        <source>Delete plugin</source>
+        <translation>Plugin löschen</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="797"/>
+        <source>Show this panel at</source>
+        <translation>Zeige dieses Panel</translation>
+    </message>
+    <message>
+        <location filename="../razorpanel.cpp" line="840"/>
+        <source>Configure panel</source>
+        <translation>Panel konfigurieren</translation>
+    </message>
+</context>
+</TS>
