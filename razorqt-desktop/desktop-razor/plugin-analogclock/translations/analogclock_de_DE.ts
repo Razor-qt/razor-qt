82bcfb2b304e9a1d3ff2a76215e48c7847b2a288
Egon Ashrafinia <e.ashrafinia@gmail.com>
2011-12-27 13:39:35 +0100

    Initial german [de] translation

---
+++ razorqt-desktop/desktop-razor/plugin-analogclock/translations/analogclock_de_DE.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>Clock</name>
+    <message>
+        <location filename="../clock.cpp" line="91"/>
+        <source>Clock</source>
+        <translation>Uhr</translation>
+    </message>
+</context>
+<context>
+    <name>ConfigureDialog</name>
+    <message>
+        <location filename="../configuredialog.ui" line="14"/>
+        <source>Analog clock settings</source>
+        <translation>Analoge Uhr Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="../configuredialog.ui" line="23"/>
+        <source>Show &amp;seconds hand</source>
+        <translation>Zeige &amp;Sekungenzeiger </translation>
+    </message>
+</context>
+</TS>
