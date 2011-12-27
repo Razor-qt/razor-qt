ef5dd6ea29e42589cb07747f0eccd9787250843e
Wubbbi <e.ashrafinia@gmail.com>
2011-12-27 03:36:58 +0100

    More translation to german

---
+++ razorqt-panel/plugin-quicklaunch/translations/razorpanel_quicklaunch_de_DE.ts
@@ -0,0 +1,35 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>QuickLaunchButton</name>
+    <message>
+        <location filename="../quicklaunchbutton.cpp" line="54"/>
+        <source>Move left</source>
+        <translation>Nach links bewegen</translation>
+    </message>
+    <message>
+        <location filename="../quicklaunchbutton.cpp" line="57"/>
+        <source>Move right</source>
+        <translation>Nach rechts bewegen</translation>
+    </message>
+    <message>
+        <location filename="../quicklaunchbutton.cpp" line="61"/>
+        <source>Remove from quicklaunch</source>
+        <translation>Aus dem Schnellstarter löschen</translation>
+    </message>
+</context>
+<context>
+    <name>RazorQuickLaunch</name>
+    <message>
+        <location filename="../razorquicklaunch.cpp" line="175"/>
+        <source>Drop Error</source>
+        <translation>Drop Error</translation>
+    </message>
+    <message>
+        <location filename="../razorquicklaunch.cpp" line="176"/>
+        <source>File/URL &apos;%1&apos; cannot be embedded into QuickLaunch for now</source>
+        <translation>Datei/Pfad &apos;%1&apos; kann im moment nicht in den Schnellstarter integriert werden</translation>
+    </message>
+</context>
+</TS>
