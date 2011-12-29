45a5941f3e75227ea6e606569e6730d0268c8db8
Egon Ashrafinia <e.ashrafinia@gmail.com>
2011-12-29 11:05:21 +0100

    Update german [de] translation

---
+++ libraries/qtxdg/translations/qtxdg_de_DE.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>XdgMenuReader</name>
+    <message>
+        <location filename="../xdgmenureader.cpp" line="74"/>
+        <source>Menu file not defined.</source>
+        <translation>Menü Datei nicht definiert.</translation>
+    </message>
+    <message>
+        <location filename="../xdgmenureader.cpp" line="91"/>
+        <source>%1 not loading: %2</source>
+        <translation>%1 läd nicht: %2</translation>
+    </message>
+    <message>
+        <location filename="../xdgmenureader.cpp" line="103"/>
+        <source>Parse error at line %1, column %2:
+%3</source>
+        <translation>Fehler in der Zeile %1, Spalte %2: %3</translation>
+    </message>
+</context>
+</TS>
