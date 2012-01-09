df4c92256b07cf1ecdde50fefccc1d72382a757e
Alexander Sokolov <sokoloff.a@gmail.com>
2012-01-10 00:44:13 +0400

    LGPL2+ for qtxdg

--- libraries/qtxdg/xdgdesktopfile.cpp
+++ libraries/qtxdg/xdgdesktopfile.cpp
@@ -1,17 +1,17 @@
 /* BEGIN_COMMON_COPYRIGHT_HEADER
- * (c)LGPL3+
+ * (c)LGPL2+
  *
  * Razor - a lightweight, Qt based, desktop toolset
  * http://razor-qt.org
  *
  * Copyright: 2010-2011 Razor team
  * Authors:
- *   Alexander Sokoloff <sokoloff.a@gmail.ru>
+ *   Alexander Sokoloff <sokoloff.a@gmail.com>
  *
  * This program or library is free software; you can redistribute it
  * and/or modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- libraries/qtxdg/xdgdesktopfile.h
+++ libraries/qtxdg/xdgdesktopfile.h
@@ -1,17 +1,17 @@
 /* BEGIN_COMMON_COPYRIGHT_HEADER
- * (c)LGPL3+
+ * (c)LGPL2+
  *
  * Razor - a lightweight, Qt based, desktop toolset
  * http://razor-qt.org
  *
  * Copyright: 2010-2011 Razor team
  * Authors:
- *   Alexander Sokoloff <sokoloff.a@gmail.ru>
+ *   Alexander Sokoloff <sokoloff.a@gmail.com>
  *
  * This program or library is free software; you can redistribute it
  * and/or modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -51,7 +51,7 @@ class XdgDesktopFileData;
  http://standards.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
 
  Note that not all methods in this class make sense for all types of desktop files.
- \author Alexander Sokoloff <sokoloff.a@gmail.ru>
+ \author Alexander Sokoloff <sokoloff.a@gmail.com>
  */
 
 class XdgDesktopFile
