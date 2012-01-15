1bccfb70ef10d053fb5d72294237596940f40bbf
Benoit Walter <b.walter@free.fr>
2012-01-15 22:08:33 +0100

    Added missing razordesktopapplication.h/.cpp.

---
+++ razorqt-desktop/src/razordesktopapplication.h
@@ -0,0 +1,50 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * Razor - a lightweight, Qt based, desktop toolset
+ * http://razor-qt.org
+ *
+ * Copyright: 2010-2011 Razor team
+ * Authors:
+ *   Benoit Walter <b.walter@free.fr>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+
+#ifndef RAZORDESKTOPAPPLICATION_H
+#define RAZORDESKTOPAPPLICATION_H
+
+#include <QApplication>
+
+class DesktopPlugin;
+class RazorDesktop;
+
+class RazorDesktopApplication : public QApplication
+{
+public:
+    explicit RazorDesktopApplication(int& argc, char** argv);
+    virtual bool x11EventFilter(XEvent* event);
+
+    DesktopPlugin* desktopPlugin() const { return m_desktopPlugin; }
+    void setDesktopPlugin(DesktopPlugin*);
+
+private:
+    DesktopPlugin* m_desktopPlugin;
+};
+
+#endif // RAZORDESKTOPAPPLICATION_H
