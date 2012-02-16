cd5979118a6b77202ab53d2c9caf8f6173b135f0
bartek <bartek@bartekpc.dom>
2012-02-16 20:50:33 +0100

    Changing box layout to grid layout in quick launch plugin
    Signed-off-by: Bartek Taczala <w0lanto@gmail.com>

--- razorqt-panel/plugin-quicklaunch/razorquicklaunch.cpp
+++ razorqt-panel/plugin-quicklaunch/razorquicklaunch.cpp
@@ -48,10 +48,17 @@
 
 EXPORT_RAZOR_PANEL_PLUGIN_CPP(RazorQuickLaunch)
 
+namespace
+{
+    static const unsigned int g_scNumberOfRows = 2 ;
+}
+
 
 RazorQuickLaunch::RazorQuickLaunch(const RazorPanelPluginStartInfo* startInfo, QWidget* parent)
     : RazorPanelPlugin(startInfo, parent),
-      m_maxIndex(0)
+      m_maxIndex(0),
+      m_currentRow(0),
+      m_currentColumn(0)
 {
     setObjectName("QuickLaunch");
     setAcceptDrops(true);
@@ -122,7 +129,13 @@ int RazorQuickLaunch::countOfButtons() const
 void RazorQuickLaunch::addButton(QuickLaunchAction* action)
 {
     QuickLaunchButton* btn = new QuickLaunchButton(m_maxIndex, action, this);
-    m_layout->addWidget(btn);
+    m_layout->addWidget(btn,m_currentRow,m_currentColumn++);
+    if ( m_currentColumn == g_scNumberOfRows)
+    {
+        m_currentColumn = 0 ;
+        m_currentRow++;
+    }
+
     m_buttons[m_maxIndex] = btn;
     
     connect(btn, SIGNAL(switchButtons(int,int)), this, SLOT(switchButtons(int,int)));
