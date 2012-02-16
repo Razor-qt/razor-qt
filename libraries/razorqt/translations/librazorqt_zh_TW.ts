413befaa54d31af4c4763d18510dc1e12cfc1e91
unknown <Administrator@speed.(none)>
2012-02-17 01:19:18 +0800

    Add Traditional Chinese Translations

---
+++ libraries/razorqt/translations/librazorqt_zh_TW.ts
@@ -0,0 +1,204 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>AddPluginDialog</name>
+    <message>
+        <location filename="../addplugindialog/addplugindialog.ui" line="14"/>
+        <source>Add plugins</source>
+        <translation>新增小工具</translation>
+    </message>
+    <message>
+        <location filename="../addplugindialog/addplugindialog.ui" line="22"/>
+        <source>Search:</source>
+        <translation>搜尋：</translation>
+    </message>
+    <message>
+        <location filename="../addplugindialog/addplugindialog.ui" line="52"/>
+        <source>Add plugin</source>
+        <translation>新增小工具</translation>
+    </message>
+    <message>
+        <location filename="../addplugindialog/addplugindialog.ui" line="59"/>
+        <source>Close</source>
+        <translation>關閉</translation>
+    </message>
+</context>
+<context>
+    <name>PowerManager</name>
+    <message>
+        <location filename="../powermanager.cpp" line="96"/>
+        <source>Hibernate</source>
+        <translation>休眠</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="103"/>
+        <source>Suspend</source>
+        <translation>暫停</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="110"/>
+        <source>Reboot</source>
+        <translation>重新啟動</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="117"/>
+        <source>Shutdown</source>
+        <translation>關機</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="124"/>
+        <source>Logout</source>
+        <translation>登出</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="137"/>
+        <source>Razor Session Suspend</source>
+        <translation>暫停Razor會話</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="138"/>
+        <source>Do you want to really suspend your computer?&lt;p&gt;Suspends the computer into a low power state. System state is not preserved if the power is lost.</source>
+        <translation>你確定要讓您的電腦進入暫停模式嗎？&lt;p&gt;暫停主機可以節省耗能。切斷電源後系統狀況也將遺失。</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="149"/>
+        <source>Razor Session Hibernate</source>
+        <translation>休眠Razor會話</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="150"/>
+        <source>Do you want to really hibernate your computer?&lt;p&gt;Hibernates the computer into a low power state. System state is preserved if the power is lost.</source>
+        <translation>您確定要讓電腦進入休眠模式嗎？&lt;p&gt;休眠主機可以節省耗能。切斷電源後系統狀況也將遺失。</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="161"/>
+        <source>Razor Session Reboot</source>
+        <translation>重新啟動Razor會話</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="162"/>
+        <source>Do you want to really restart your computer? All unsaved work will be lost...</source>
+        <translation>您確定要重新啟動您的電腦嗎？所有未儲存工作將會遺失……</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="173"/>
+        <source>Razor Session Shutdown</source>
+        <translation>關閉電源</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="174"/>
+        <source>Do you want to really switch off your computer? All unsaved work will be lost...</source>
+        <translation>您確定要關閉您的電腦嗎？所有未儲存工作將會遺失……</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="185"/>
+        <source>Razor Session Logout</source>
+        <translation>登出Razor會話</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="186"/>
+        <source>Do you want to really logout? All unsaved work will be lost...</source>
+        <translation>您確定要登出嗎？所有未儲存工作將會遺失……</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="197"/>
+        <location filename="../powermanager.cpp" line="202"/>
+        <source>Razor Power Manager Error</source>
+        <translation>Razor電源管理員錯誤</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="197"/>
+        <source>Hibernate failed.</source>
+        <translation>休眠失敗。</translation>
+    </message>
+    <message>
+        <location filename="../powermanager.cpp" line="202"/>
+        <source>Suspend failed.</source>
+        <translation>暫停失敗。</translation>
+    </message>
+</context>
+<context>
+    <name>RazorConfigDialog</name>
+    <message>
+        <location filename="../razorconfigdialog.ui" line="6"/>
+        <source>Dialog</source>
+        <translation>對話框</translation>
+    </message>
+</context>
+<context>
+    <name>RazorShortcutButton</name>
+    <message>
+        <location filename="../razorshortcutbutton.cpp" line="49"/>
+        <source>Clear</source>
+        <translation>清除</translation>
+    </message>
+    <message>
+        <location filename="../razorshortcutbutton.cpp" line="199"/>
+        <source>None</source>
+        <translation>無</translation>
+    </message>
+</context>
+<context>
+    <name>ScreenSaver</name>
+    <message>
+        <location filename="../screensaver.cpp" line="49"/>
+        <source>Lock Screen</source>
+        <translation>螢幕上鎖</translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="68"/>
+        <location filename="../screensaver.cpp" line="76"/>
+        <location filename="../screensaver.cpp" line="84"/>
+        <location filename="../screensaver.cpp" line="96"/>
+        <location filename="../screensaver.cpp" line="104"/>
+        <location filename="../screensaver.cpp" line="112"/>
+        <location filename="../screensaver.cpp" line="121"/>
+        <source>Screen Saver Activation Error</source>
+        <translation>啟動螢幕保護程序錯誤</translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="69"/>
+        <source>An error occurred starting screensaver. xdg-screensaver cannot be started due its crash.</source>
+        <translation>啟動螢幕保護時發生錯誤：因為xdg-screensaver崩潰而無法啟動</translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="77"/>
+        <source>An error occurred starting screensaver. xdg-screensaver is not installed correctly.</source>
+        <translation>啟動螢幕保護時發生錯誤：xdg-screensaver並沒有正確安裝。</translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="85"/>
+        <source>An error occurred starting screensaver. xdg-screensaver cannot be started.</source>
+        <translation>啟動螢幕保護時發生錯誤：xdg-screensaver無法啟動。</translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="97"/>
+        <source>An error occurred starting screensaver. Syntax error in xdg-screensaver arguments.</source>
+        <translation>啟動螢幕保護時發生錯誤：xdg-screensaver參數語法錯誤。</translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="105"/>
+        <source>An error occurred starting screensaver. Ensure you have xscreensaver installed and running.</source>
+        <translation>啟動螢幕保護時發生錯誤：請確定您安裝並運行了xscreensaver。</translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="113"/>
+        <source>An error occurred starting screensaver. Action &apos;activate&apos; failed. Ensure you have xscreensaver installed and running.</source>
+        <translation>啟動螢幕保護時發生錯誤：啟動失敗。請確定您安裝並運行了xscreensaver。</translation>
+    </message>
+    <message>
+        <location filename="../screensaver.cpp" line="122"/>
+        <source>An error occurred starting screensaver. Unkown error - undocumented return value from xdg-screensaver=%1.</source>
+        <translation>啟動螢幕保護時發生錯誤：未知錯誤，從xdg-screensaver=%1返回的值未歸檔。</translation>
+    </message>
+</context>
+<context>
+    <name>about</name>
+    <message>
+        <location filename="../razoraboutdlg.ui" line="14"/>
+        <source> About Razor</source>
+        <translation>關於Razor</translation>
+    </message>
+</context>
+</TS>
