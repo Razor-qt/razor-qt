0bc606288ea3296f68290b98b9f70571d25377b9
stuarch <Administrator@speed.(none)>
2012-02-22 17:42:28 +0800

    Update Tradidtional Chinese Translations

---
+++ razorqt-session/translations/razor-session_zh_TW.ts
@@ -0,0 +1,267 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>AutoStartEdit</name>
+    <message>
+        <location filename="../config/autostartedit.ui" line="14"/>
+        <source>Edit</source>
+        <translation>編輯</translation>
+    </message>
+    <message>
+        <location filename="../config/autostartedit.ui" line="20"/>
+        <source>Name:</source>
+        <translation>名稱:</translation>
+    </message>
+    <message>
+        <location filename="../config/autostartedit.ui" line="27"/>
+        <source>Search...</source>
+        <translation>搜尋...</translation>
+    </message>
+    <message>
+        <location filename="../config/autostartedit.ui" line="47"/>
+        <source>Command:</source>
+        <translation>指令:</translation>
+    </message>
+    <message>
+        <location filename="../config/autostartedit.cpp" line="70"/>
+        <source>Select Application</source>
+        <translation>選擇應用程式</translation>
+    </message>
+</context>
+<context>
+    <name>AutoStartItemModel</name>
+    <message>
+        <location filename="../config/autostartmodel.cpp" line="156"/>
+        <source>Global Autostart</source>
+        <translation>系統自動啟動</translation>
+    </message>
+    <message>
+        <location filename="../config/autostartmodel.cpp" line="158"/>
+        <source>Razor Autostart</source>
+        <translation>Razor自動啟動</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="39"/>
+        <source>Openbox - light-weight window manager</source>
+        <translation>Openbox - 輕巧的視窗管理員</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="40"/>
+        <source>KWin - window manager of the KDE Software Compilation</source>
+        <translation>KWin - KDE軟體集中所包涵的視窗管理員</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="41"/>
+        <source>Metacity - window manager of the GNOME desktop environment</source>
+        <translation>Metacity - GNOME桌面環境所包涵的視窗管理員</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="42"/>
+        <source>Windowmaker - a classical lightweight window manager</source>
+        <translation>Windowmaker - 一个经典的轻量级窗口管理器</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="43"/>
+        <source>Enlightenement 16</source>
+        <translation>Enlightenement 16</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="44"/>
+        <source>Fvwm2 - a classical lightweight window manager</source>
+        <translation>Fvwm2 - 一個經典的輕巧視窗管理員</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="45"/>
+        <source>Xfwm4 - a window manager for the Xfce desktop environment</source>
+        <translation>Xfwm4 - Xfce4桌面環境所包涵的視窗管理員</translation>
+    </message>
+    <message>
+        <location filename="../src/windowmanager.cpp" line="46"/>
+        <source>Sawfish - a lightweight, flexible window manager</source>
+        <translation>Sawfish - 輕巧且靈活的視窗管理員</translation>
+    </message>
+</context>
+<context>
+    <name>RazorModuleManager</name>
+    <message>
+        <location filename="../src/razormodman.cpp" line="179"/>
+        <source>Razor Session Crash Report</source>
+        <translation>Razor會話錯誤回報</translation>
+    </message>
+    <message>
+        <location filename="../src/razormodman.cpp" line="180"/>
+        <source>Application &apos;%1&apos; crashed too many times. Its autorestart has been disabled for current session.</source>
+        <translation>應用程式 &apos;%1&apos; 不正常結束過於頻繁。所以它在此會話中的自動啟動將被取消。</translation>
+    </message>
+</context>
+<context>
+    <name>SessionConfigWindow</name>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="14"/>
+        <source>Razor Session Configuration</source>
+        <translation>Razor會話設定</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="58"/>
+        <source>Window Manager</source>
+        <translation>視窗管理員</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="77"/>
+        <location filename="../config/sessionconfigwindow.ui" line="234"/>
+        <location filename="../config/sessionconfigwindow.ui" line="261"/>
+        <source>Search...</source>
+        <translation>搜尋...</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="87"/>
+        <source>Razor Modules</source>
+        <translation>Razor模組</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="111"/>
+        <source>Panel</source>
+        <translation>面板</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="118"/>
+        <source>Desktop</source>
+        <translation>桌面</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="125"/>
+        <source>Runner</source>
+        <translation>啟動器</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="132"/>
+        <source>Application Switcher</source>
+        <translation>應用程式切換器</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="152"/>
+        <source>PolicyKit Handler</source>
+        <translation>權限管理處理程序</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="172"/>
+        <location filename="../config/sessionconfigwindow.cpp" line="53"/>
+        <source>Basic Settings</source>
+        <translation>基本設定</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="189"/>
+        <location filename="../config/sessionconfigwindow.cpp" line="54"/>
+        <source>Default Applications</source>
+        <translation>預設應用程式</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="214"/>
+        <source>Terminal Emulation:</source>
+        <translation>終端模擬機:</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="241"/>
+        <source>Web Browser:</source>
+        <translation>網頁瀏覽器:</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="289"/>
+        <location filename="../config/sessionconfigwindow.ui" line="380"/>
+        <source>Delete</source>
+        <translation>刪除</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="296"/>
+        <location filename="../config/sessionconfigwindow.ui" line="373"/>
+        <source>Add</source>
+        <translation>新增</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="303"/>
+        <source>Edit</source>
+        <translation>編輯</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="316"/>
+        <source>Application Autostart</source>
+        <translation>自動啟動應用程式</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="406"/>
+        <source>Environment Variables (Advanced)</source>
+        <translation>環境參數 (進階)</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="417"/>
+        <source>Variable Name</source>
+        <translation>參數名稱</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.ui" line="422"/>
+        <source>Value</source>
+        <translation>數值</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.cpp" line="55"/>
+        <source>Autostart</source>
+        <translation>自動啟動</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.cpp" line="56"/>
+        <source>Environment (Advanced)</source>
+        <translation>環境 (進階)</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.cpp" line="196"/>
+        <source>Session Restart Required</source>
+        <translation>會話重新啟動請求</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.cpp" line="197"/>
+        <source>You need to restart desktop session (razor-session) to reload settings. Use logout from the main menu.</source>
+        <translation>您必須重新啟動您的桌面會話(Razor會話)來套用設定。在主目錄中使用登出選項。</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.cpp" line="242"/>
+        <source>Select a window manager</source>
+        <translation>選擇視窗管理員</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.cpp" line="248"/>
+        <source>Select a terminal emulator</source>
+        <translation>選擇終端模擬器</translation>
+    </message>
+    <message>
+        <location filename="../config/sessionconfigwindow.cpp" line="254"/>
+        <source>Select a web browser</source>
+        <translation>選擇網頁瀏覽器</translation>
+    </message>
+</context>
+<context>
+    <name>WmSelectDialog</name>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="14"/>
+        <source>Welcome to Razor-qt</source>
+        <translation>歡迎使用 Razor-qt</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="20"/>
+        <source>&lt;b&gt;Welcome to Razor-qt&lt;/b&gt;
+&lt;p&gt;
+Before starting to use the Razor-qt, you might want to select the Windows Manager:</source>
+        <translation>&lt;b&gt;歡迎使用 Razor-qt&lt;/b&gt;
+&lt;p&gt;
+在開始探索Razor-qt之前，您也許會想先選擇視窗管理器:</translation>
+    </message>
+    <message>
+        <location filename="../src/wmselectdialog.ui" line="35"/>
+        <source>You can change the Window Manager later at any time via Razor Session Configurator.</source>
+        <translation>您以在任何時間透過Razor會話設定更改視窗管理員。</translation>
+    </message>
+</context>
+</TS>
