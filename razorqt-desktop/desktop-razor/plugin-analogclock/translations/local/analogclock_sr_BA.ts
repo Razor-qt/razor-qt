3361eac6bd8ee4684ddda84c54dbfa6f8567066c
Alexander Sokolov <sokoloff.a@gmail.com>
2012-02-24 01:35:18 +0400

    Transifex desktop: local translations

---
+++ razorqt-desktop/desktop-razor/plugin-analogclock/translations/local/analogclock_sr_BA.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sr_BA">
+<context>
+    <name>ConfigureDialog</name>
+    <message>
+        <source>Analog clock settings</source>
+        <translation>Подешавање аналогног сата</translation>
+    </message>
+    <message>
+        <source>Show &amp;seconds hand</source>
+        <translation>Прикажи &amp;секундару</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Clock</source>
+        <translation>Сат</translation>
+    </message>
+</context>
+</TS>
---
+++ razorqt-panel/plugin-showdesktop/translations/local/showdesktop_sr_BA.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sr_BA">
+<context>
+    <name>ShowDesktop</name>
+    <message>
+        <source>Global keyboard shortcut</source>
+        <translation>Глобална пречица тастатуре</translation>
+    </message>
+    <message>
+        <source>Panel Show Desktop Global shorcut: &apos;%1&apos; cannot be registered</source>
+        <translation>Глобална пречица приказа површи за панел: „%1“ не може бити регистрована</translation>
+    </message>
+    <message>
+        <source>Show Desktop</source>
+        <translation>Прикажи радну површ</translation>
+    </message>
+</context>
+</TS>
