48af56e95bb608834b887c171a5e3d14d9718f25
Petr Vanek <petr@scribus.info>
2012-06-12 16:36:31 +0200

    notification icon fix

--- razorqt-notificationd/src/notification.cpp
+++ razorqt-notificationd/src/notification.cpp
@@ -78,29 +78,30 @@ void Notification::setValues(const QString &application,
     if (!hints["image_data"].isNull())
     {
         m_pixmap = getPixmapFromHint(hints["image_data"]);
-        //qDebug() << application << "from image_data" << m_pixmap.isNull();
+//        qDebug() << application << "from image_data" << m_pixmap.isNull();
     }
     else if (!hints["image_path"].isNull())
     {
         m_pixmap = getPixmapFromString(hints["image_path"].toString());
-        //qDebug() << application << "from image_path" << m_pixmap.isNull();
+//        qDebug() << application << "from image_path" << m_pixmap.isNull();
     }
     else if (!icon.isEmpty())
     {
         m_pixmap = getPixmapFromString(icon);
-        //qDebug() << application << "from icon" << icon << m_pixmap.isNull();
+//        qDebug() << application << "from icon" << icon << m_pixmap.isNull();
     }
     else if (!hints["icon_data"].isNull())
     {
        m_pixmap = getPixmapFromHint(hints["icon_data"]);
-       //qDebug() << application << "from icon_data" << m_pixmap.isNull();
+//       qDebug() << application << "from icon_data" << m_pixmap.isNull();
     }
     // failback
     if (m_pixmap.isNull())
     {
-        qDebug() << "An icon for name:" << icon << "or hints" << hints << "not found. Using failback.";
-        //m_pixmap = XdgIcon::defaultApplicationIcon().pixmap(ICONSIZE);
+//        qDebug() << "An icon for name:" << icon << "or hints" << hints << "not found. Using failback.";
+        m_pixmap = XdgIcon::defaultApplicationIcon().pixmap(ICONSIZE);
     }
+    iconLabel->setPixmap(m_pixmap);
 
     // application
     appLabel->setText(application);
@@ -203,13 +204,13 @@ QPixmap Notification::getPixmapFromString(const QString &str) const
     QUrl url(str);
     if (url.isValid() && QFile::exists(url.toLocalFile()))
     {
-        //qDebug() << "    getPixmapFromString by URL" << url;
+//        qDebug() << "    getPixmapFromString by URL" << url;
         return QPixmap(url.toLocalFile());
     }
     else
     {
-        //qDebug() << "    getPixmapFromString by XdgIcon theme" << str << ICONSIZE << XdgIcon::themeName();
-        //qDebug() << "       " << XdgIcon::fromTheme(str);
-        return XdgIcon::fromTheme(str).pixmap(ICONSIZE);
+//        qDebug() << "    getPixmapFromString by XdgIcon theme" << str << ICONSIZE << XdgIcon::themeName();
+//        qDebug() << "       " << XdgIcon::fromTheme(str) << "isnull:" << XdgIcon::fromTheme(str).isNull();
+        return XdgIcon::fromTheme(str, XdgIcon::defaultApplicationIcon()).pixmap(ICONSIZE);
     }
 }
