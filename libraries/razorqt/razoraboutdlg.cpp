3f0988c84f70e4479f42913315548bd2bb78bd0f
Petr Vanek <petr@scribus.info>
2012-06-26 10:46:21 +0200

    extended "razor-about" implementation. Resolves #253

--- libraries/razorqt/razoraboutdlg.cpp
+++ libraries/razorqt/razoraboutdlg.cpp
@@ -30,6 +30,7 @@
 #include "razoraboutdlg.h"
 #include "ui_razoraboutdlg.h"
 #include "libtranslate.h"
+#include <qtxdg/xdgdirs.h>
 
 /**
  * @brief prepares the data to show and fills the form, then shows.
@@ -46,7 +47,9 @@ RazorAboutDLGPrivate::RazorAboutDLGPrivate()
 {
     libTranslate("librazorqt");
     setupUi(this);
-    QString content = QString::fromUtf8("<html><body>"
+
+    // about razor
+    QString about = QString::fromUtf8("<html><body>"
     "<h1>Razor Desktop Toolbox<img src=\"images://razor_logo.png\" align=\"right\" /></h1>"
     "<p>Version: %1</p>"
     "<p>Homepage: <a href=\"http://razor-qt.org\">razor-qt.org</a></p>"
@@ -78,10 +81,43 @@ RazorAboutDLGPrivate::RazorAboutDLGPrivate()
     
     QTextDocument * doc = new QTextDocument(this);
     doc->addResource(QTextDocument::ImageResource, QUrl("images://razor_logo.png"), QPixmap(QString(SHARE_DIR) + "/graphics/razor_logo.png"));
-    doc->setHtml(content.arg(RAZOR_VERSION));
+    doc->setHtml(about.arg(RAZOR_VERSION));
+    aboutBrowser->setDocument(doc);
+
+    // technical info
+#ifdef DEBUG
+    QString debug(tr("Yes"));
+#else
+    QString debug(tr("No"));
+#endif
+    XdgDirs xdgDirs;
+    QString tech = QString::fromUtf8("<html><body>");
+            tech += tr("<h1>Razor Desktop Toolbox - Technical Info</h1>");
+            tech += "<table width=\"80%\">";
+            tech += tr("<tr><th>Version:</th><td>%1</td></tr>").arg(RAZOR_VERSION);
+            tech += tr("<tr><th>Qt:</th><td>%1</td></tr>").arg(qVersion());
+            tech += tr("<tr><th>Debug Build:</th><td>%1</td></tr>").arg(debug);
+            tech += tr("<tr><th>System Configuration:</th><td>%1</td></tr>").arg(RAZOR_ETC_XDG_DIRECTORY);
+            tech += tr("<tr><th>Share Directory:</th><td>%1</td></tr>").arg(SHARE_DIR);
+            tech += tr("<tr><th>Translations:</th><td>%1</td></tr>").arg(TRANSLATIONS_DIR);
+            tech += "</table>";
+            tech += tr("<h2>User Directories</h2>");
+            tech += "<table width=\"80%\">";
+            tech += tr("<tr><th>Xdg Data Home:</th><td>%1</td></tr>").arg(xdgDirs.dataHome(false));
+            tech += tr("<tr><th>Xdg Config Home:</th><td>%1</td></tr>").arg(xdgDirs.configHome(false));
+            tech += tr("<tr><th>Xdg Data Dirs:</th><td>%1</td></tr>").arg(xdgDirs.dataDirs().join(":"));
+            tech += tr("<tr><th>Xdg Cache Home:</th><td>%1</td></tr>").arg(xdgDirs.cacheHome(false));
+            tech += tr("<tr><th>Xdg Runtime Home:</th><td>%1</td></tr>").arg(xdgDirs.runtimeDir());
+            tech += tr("<tr><th>Xdg Autostart Dirs:</th><td>%1</td></tr>").arg(xdgDirs.autostartDirs().join(":"));
+            tech += tr("<tr><th>Xdg Autostart Home:</th><td>%1</td></tr>").arg(xdgDirs.autostartHome(false));
+            tech += "</table>";
+            tech += "</body></html>";
+
+    doc = new QTextDocument(this);
+    doc->setHtml(tech);
+    techBrowser->setDocument(doc);
 
     this->setAttribute(Qt::WA_DeleteOnClose);
-    textBrowser->setDocument(doc);
     show();
 }
 
---
+++ razorqt-about/src/translations/translate.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export PROJECT=razor-power
+export OPTS="-noobsolete"
+export TARGET="-recursive .."
+
+../../../scripts/translate-one.sh "$@"
