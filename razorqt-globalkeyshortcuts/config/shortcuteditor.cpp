805765928c201cc3e59b10e7fc1f5fc8370a8a73
Petr Vanek <petr@scribus.info>
2012-06-29 12:57:19 +0200

    globalkeys: missing license; display groups as a whole line in the config dialog

--- razorqt-globalkeyshortcuts/config/shortcuteditor.cpp
+++ razorqt-globalkeyshortcuts/config/shortcuteditor.cpp
@@ -1,3 +1,30 @@
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
+ *
+ * Razor - a lightweight, Qt based, desktop toolset
+ * http://razor-qt.org
+ *
+ * Copyright: 2012 Razor team
+ * Authors:
+ *   Aaron Lewis <the.warl0ck.1989@gmail.com>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
 #include "shortcuteditor.h"
 
 #define D_GROUP(q) (QMessageBox::question(this , tr("Question") , tr("Delete group: %1 (everything inside will be removed altogether) ?").arg (q) , QMessageBox::Yes , QMessageBox::No) == QMessageBox::No)
@@ -194,6 +221,7 @@ void ShortcutEditor::loadSettings()
             {
                 parentItem = new QTreeWidgetItem(QStringList() << groupName);
                 mTreeWidget->insertTopLevelItem(mTreeWidget->topLevelItemCount() , parentItem);
+                mTreeWidget->setFirstItemColumnSpanned(parentItem, true);
 
                 groupItems.insert(groupName , parentItem);
             }
