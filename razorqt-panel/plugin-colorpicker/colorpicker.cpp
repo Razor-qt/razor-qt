f9c586efc1ea8c1d90ccac2d75aa4483e6bbf8c5
c0debreak <the.warl0ck.1989@gmail.com>
2012-07-02 17:54:31 +0800

    Fix indent

--- razorqt-panel/plugin-colorpicker/colorpicker.cpp
+++ razorqt-panel/plugin-colorpicker/colorpicker.cpp
@@ -1,48 +1,48 @@
 /* BEGIN_COMMON_COPYRIGHT_HEADER
- * (c)LGPL2+
- *
- * Razor - a lightweight, Qt based, desktop toolset
- * http://razor-qt.org
- *
- * Copyright: 2012 Razor team
- * Authors:
- *   Aaron Lewis <the.warl0ck.1989@gmail.com>
- *
- * This program or library is free software; you can redistribute it
- * and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+    * (c)LGPL2+
+    *
+    * Razor - a lightweight, Qt based, desktop toolset
+    * http://razor-qt.org
+    *
+    * Copyright: 2012 Razor team
+    * Authors:
+    *   Aaron Lewis <the.warl0ck.1989@gmail.com>
+    *
+    * This program or library is free software; you can redistribute it
+    * and/or modify it under the terms of the GNU Lesser General Public
+    * License as published by the Free Software Foundation; either
+    * version 2.1 of the License, or (at your option) any later version.
+    *
+    * This library is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    * Lesser General Public License for more details.
 
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
- *
- * END_COMMON_COPYRIGHT_HEADER */
+    * You should have received a copy of the GNU Lesser General
+    * Public License along with this library; if not, write to the
+    * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    * Boston, MA 02110-1301 USA
+    *
+    * END_COMMON_COPYRIGHT_HEADER */
 #include "colorpicker.h"
 
 EXPORT_RAZOR_PANEL_PLUGIN_CPP (ColorPicker)
 
 ColorPicker::ColorPicker(const RazorPanelPluginStartInfo* startInfo, QWidget* parent):
-	RazorPanelPlugin (startInfo , parent)
+        RazorPanelPlugin (startInfo , parent)
 {
-	setObjectName ("ColorPicker");
+    setObjectName ("ColorPicker");
 
-	QFontMetrics fm (font());
-	lineEdit.setFixedWidth ( 10*fm.width ("a") );
+    QFontMetrics fm (font());
+    lineEdit.setFixedWidth ( 10*fm.width ("a") );
 
-	layout()->addWidget (&button);
-	layout()->addWidget (&lineEdit);
+    layout()->addWidget (&button);
+    layout()->addWidget (&lineEdit);
 
-	button.setIcon ( XdgIcon::fromTheme("kcolorchooser") );
+    button.setIcon ( XdgIcon::fromTheme("kcolorchooser") );
 
-	connect (&button , SIGNAL(clicked()) , &thread , SLOT(start()));
-	connect (&thread , SIGNAL(started()) , SLOT(loop()));
+    connect (&button , SIGNAL(clicked()) , &thread , SLOT(start()));
+    connect (&thread , SIGNAL(started()) , SLOT(loop()));
 }
 
 ColorPicker::~ColorPicker()
@@ -56,24 +56,24 @@ void ColorPicker::loop ()
 
     d = XOpenDisplay(NULL);
     if ( ! d )
-	return;
+        return;
 
     root = DefaultRootWindow(d);
 
     XGrabPointer(d, root, False, ButtonPressMask | ButtonReleaseMask |
-	    PointerMotionMask, GrabModeAsync, GrabModeAsync, None,
-	    None, CurrentTime);
+            PointerMotionMask, GrabModeAsync, GrabModeAsync, None,
+            None, CurrentTime);
 
     XEvent ev;
     while (1) {
-	XNextEvent(d, &ev);
+        XNextEvent(d, &ev);
 
-	if (ev.type == ButtonRelease)
-	{
-	    QApplication::processEvents();
-	    takeScreenshot();
-	    break;
-	}
+        if (ev.type == ButtonRelease)
+        {
+            QApplication::processEvents();
+            takeScreenshot();
+            break;
+        }
     }
 
     XCloseDisplay(d);
@@ -87,7 +87,6 @@ void ColorPicker::takeScreenshot ()
     QPixmap pixmap = QPixmap::grabWindow(QApplication::desktop()->winId(), pos.x(), pos.y(), 1, 1);
 
     QImage img = pixmap.toImage();
-    // image has only one pixel at coords (0,0), get its color
     QColor col = QColor(img.pixel(0,0));
 
     lineEdit.setText (col.name());
--- razorqt-panel/plugin-colorpicker/colorpicker.h
+++ razorqt-panel/plugin-colorpicker/colorpicker.h
@@ -1,29 +1,29 @@
 /* BEGIN_COMMON_COPYRIGHT_HEADER
- * (c)LGPL2+
- *
- * Razor - a lightweight, Qt based, desktop toolset
- * http://razor-qt.org
- *
- * Copyright: 2012 Razor team
- * Authors:
- *   Aaron Lewis <the.warl0ck.1989@gmail.com>
- *
- * This program or library is free software; you can redistribute it
- * and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+    * (c)LGPL2+
+    *
+    * Razor - a lightweight, Qt based, desktop toolset
+    * http://razor-qt.org
+    *
+    * Copyright: 2012 Razor team
+    * Authors:
+    *   Aaron Lewis <the.warl0ck.1989@gmail.com>
+    *
+    * This program or library is free software; you can redistribute it
+    * and/or modify it under the terms of the GNU Lesser General Public
+    * License as published by the Free Software Foundation; either
+    * version 2.1 of the License, or (at your option) any later version.
+    *
+    * This library is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    * Lesser General Public License for more details.
 
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
- *
- * END_COMMON_COPYRIGHT_HEADER */
+    * You should have received a copy of the GNU Lesser General
+    * Public License along with this library; if not, write to the
+    * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+    * Boston, MA 02110-1301 USA
+    *
+    * END_COMMON_COPYRIGHT_HEADER */
 
 #ifndef RAZOR_COLORPICKER_H
 #define RAZOR_COLORPICKER_H
@@ -43,18 +43,18 @@
 class ColorPicker : public RazorPanelPlugin
 {
     Q_OBJECT
-public:
-    ColorPicker(const RazorPanelPluginStartInfo* startInfo, QWidget* parent = 0);
-    ~ColorPicker();
+    public:
+        ColorPicker(const RazorPanelPluginStartInfo* startInfo, QWidget* parent = 0);
+        ~ColorPicker();
 
-private slots:
-	void loop ();
-	void takeScreenshot ();
+        private slots:
+        void loop ();
+        void takeScreenshot ();
 
-private:
-	QLineEdit lineEdit;
-	QPushButton button;
-	QThread thread;
+    private:
+        QLineEdit lineEdit;
+        QPushButton button;
+        QThread thread;
 };
 
 EXPORT_RAZOR_PANEL_PLUGIN_H
