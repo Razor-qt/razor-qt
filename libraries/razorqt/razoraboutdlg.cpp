e9429dbcf79c393015d8e4db11ff417b88126927
Alexander Sokolov <sokoloff.a@gmail.com>
2012-09-20 21:14:01 +0400

    Translators info & refactoring of the About dialog

--- libraries/razorqt/razoraboutdlg.cpp
+++ libraries/razorqt/razoraboutdlg.cpp
@@ -31,6 +31,9 @@
 #include "ui_razoraboutdlg.h"
 #include "libtranslate.h"
 #include <qtxdg/xdgdirs.h>
+#include "translatorsinfo/translatorsinfo.h"
+#include <QDebug>
+#include <QtCore/QDate>
 
 /**
  * @brief prepares the data to show and fills the form, then shows.
@@ -39,86 +42,160 @@ class RazorAboutDLGPrivate: public Ui_about, public QDialog
 {
 public:
     RazorAboutDLGPrivate();
+    QString titleText() const;
+    QString aboutText() const;
+    QString authorsText() const;
+    QString thanksText() const;
+    QString translationsText() const;
+    QString technicalText() const;
 };
 
 
-//
 RazorAboutDLGPrivate::RazorAboutDLGPrivate()
 {
     libTranslate("librazorqt");
     setupUi(this);
 
-    // about razor
-    QString about = QString::fromUtf8("<html><body>"
-    "<h1>Razor Desktop Toolbox<img src=\"images://razor_logo.png\" align=\"right\" /></h1>"
-    "<p>Version: %1</p>"
-    "<p>Homepage: <a href=\"http://razor-qt.org\">razor-qt.org</a></p>"
-    "<h2>License: GPL2</h2>"
-    "<h2>Authors</h2>"
-    "<p>The razor-qt Crew consists of (in alphabetical order):</p>"
-    "<ul>"
-    "<li>Maciej Płaza &lt;<a href='mailto:plaza.maciej@gmail.com'>plaza.maciej@gmail.com</a>&gt;</li>"
-    "<li>Alexander Sokolov &lt;<a href='mailto:sokoloff.a@gmail.com'>sokoloff.a@gmail.com</a>&gt;</li>"
-    "<li>Petr Vanek &lt;<a href='mailto:petr@scribus.info'>petr@scribus.info</a>&gt;</li>"
-    "<li>Christopher 'VdoP' Regali (retired)</li>"
-    "</ul>"
-    "<h2>Contributors</h2>"
-    "<ul>"
-    "<li>Luis Gustavo Spern Barreto &lt;gustavosbarreto@gmail.com&gt;</li>"
-    "<li>Bartosz Taczala &lt;w0lanto@gmail.com&gt;</li>"
-    "</ul>"
-    "<p>If you want to contribute, just visit <a href=\"http://razor-qt.org\">razor-qt.org</a>.</p>"
-    "<h2>Special thanks to</h2>"
-    "<ul>"
-    "<li>Andy Fitzsimon (logo/icon)</li>"
-    "<li>Eugene Pivnev (QtDesktop)</li>"
-    "<li>Paul Gnther (for desktop backgrounds)</li>"
-    "<li>Manuel Meier (for ideas)</li>"
-    "<li>Alexander Zakher (for the name)</li>"
-    "<li>Alexey Nosov (for the A-MeGo theme)</li>"
-    "<li>and KDE (http://www.kde.org)</li>"
-    "</body></html>");
-    
-    QTextDocument * doc = new QTextDocument(this);
-    doc->addResource(QTextDocument::ImageResource, QUrl("images://razor_logo.png"), QPixmap(QString(SHARE_DIR) + "/graphics/razor_logo.png"));
-    doc->setHtml(about.arg(RAZOR_VERSION));
-    aboutBrowser->setDocument(doc);
+    QString css="<style TYPE='text/css'> "
+                    "body { font-family: Arial, Helvetica, sans-serif;} "
+                    ".name { font-size: 16pt; } "
+                    "a { white-space: nowrap ;} "
+                    "h2 { font-size: 10pt;} "
+                    "li { line-height: 120%;} "
+                    ".techInfoKey { white-space: nowrap ; margin: 0 20px 0 16px; } "
+                "</style>"
+            ;
 
-    // technical info
-#ifdef DEBUG
-    QString debug(tr("Yes"));
-#else
-    QString debug(tr("No"));
-#endif
-    XdgDirs xdgDirs;
-    QString tech = QString::fromUtf8("<html><body>");
-            tech += tr("<h1>Razor Desktop Toolbox - Technical Info</h1>");
-            tech += "<table width=\"80%\">";
-            tech += tr("<tr><th>Version:</th><td>%1</td></tr>").arg(RAZOR_VERSION);
-            tech += tr("<tr><th>Qt:</th><td>%1</td></tr>").arg(qVersion());
-            tech += tr("<tr><th>Debug Build:</th><td>%1</td></tr>").arg(debug);
-            tech += tr("<tr><th>System Configuration:</th><td>%1</td></tr>").arg(RAZOR_ETC_XDG_DIRECTORY);
-            tech += tr("<tr><th>Share Directory:</th><td>%1</td></tr>").arg(SHARE_DIR);
-            tech += tr("<tr><th>Translations:</th><td>%1</td></tr>").arg(TRANSLATIONS_DIR);
-            tech += "</table>";
-            tech += tr("<h2>User Directories</h2>");
-            tech += "<table width=\"80%\">";
-            tech += tr("<tr><th>Xdg Data Home:</th><td>%1</td></tr>").arg(xdgDirs.dataHome(false));
-            tech += tr("<tr><th>Xdg Config Home:</th><td>%1</td></tr>").arg(xdgDirs.configHome(false));
-            tech += tr("<tr><th>Xdg Data Dirs:</th><td>%1</td></tr>").arg(xdgDirs.dataDirs().join(":"));
-            tech += tr("<tr><th>Xdg Cache Home:</th><td>%1</td></tr>").arg(xdgDirs.cacheHome(false));
-            tech += tr("<tr><th>Xdg Runtime Home:</th><td>%1</td></tr>").arg(xdgDirs.runtimeDir());
-            tech += tr("<tr><th>Xdg Autostart Dirs:</th><td>%1</td></tr>").arg(xdgDirs.autostartDirs().join(":"));
-            tech += tr("<tr><th>Xdg Autostart Home:</th><td>%1</td></tr>").arg(xdgDirs.autostartHome(false));
-            tech += "</table>";
-            tech += "</body></html>";
+    iconLabel->setFixedSize(48, 48);
+    iconLabel->setScaledContents(true);
+    iconLabel->setPixmap(QPixmap(QString(SHARE_DIR) + "/graphics/razor_logo.png"));
 
-    doc = new QTextDocument(this);
-    doc->setHtml(tech);
-    techBrowser->setDocument(doc);
+    nameLabel->setText(css + titleText());
+
+    aboutBrowser->setHtml(css + aboutText());
+    aboutBrowser->viewport()->setAutoFillBackground(false);
+
+    autorsBrowser->setHtml(css + authorsText());
+    autorsBrowser->viewport()->setAutoFillBackground(false);
+
+    thanksBrowser->setHtml(css + thanksText());
+    thanksBrowser->viewport()->setAutoFillBackground(false);
+
+    translationsBrowser->setHtml(css + translationsText());
+    translationsBrowser->viewport()->setAutoFillBackground(false);
+
+    techBrowser->setHtml(css + technicalText());
+    techBrowser->viewport()->setAutoFillBackground(false);
 
     this->setAttribute(Qt::WA_DeleteOnClose);
     show();
+}
+
+QString RazorAboutDLGPrivate::titleText() const
+{
+    return QString("<div class=name>%1</div><div class=ver>%2</div>").arg(
+                "Razor-qt",
+                tr("Version: %1").arg(RAZOR_VERSION));
+
+}
+
+QString RazorAboutDLGPrivate::aboutText() const
+{
+    return  QString("<br>%1<br><br><br>%2<hr>%3<p>%4").arg(
+                tr("Advanced, easy-to-use, and fast desktop environment based on Qt technologies."),
+                tr("Copyright: %1-%2 %3").arg("2010", QDate::currentDate().toString("yyyy"), "Razor team"),
+
+                tr("Homepage: %1").arg("<a href='http://www.razor-qt.org'>www.razor-qt.org</a>"),
+                tr("License: %1").arg("<a href='http://www.gnu.org/licenses/lgpl-2.1.html'>GNU Lesser General Public License version 2.1 or later</a> "
+                                      "and partly under the "
+                                      "<a href='http://www.gnu.org/licenses/gpl-2.0.html'>GNU General Public License version 2</a>"
+                                      )
+                );
+}
+
+QString RazorAboutDLGPrivate::authorsText() const
+{
+    return  QString::fromUtf8(
+                "<b>Authors</b>"
+                "<p>The razor-qt Crew consists of (in alphabetical order):</p>"
+                "<ul>"
+                "<li>Maciej Płaza &lt;<a href='mailto:plaza.maciej@gmail.com'>plaza.maciej@gmail.com</a>&gt;</li>"
+                "<li>Alexander Sokolov &lt;<a href='mailto:sokoloff.a@gmail.com'>sokoloff.a@gmail.com</a>&gt;</li>"
+                "<li>Petr Vanek &lt;<a href='mailto:petr@scribus.info'>petr@scribus.info</a>&gt;</li>"
+                "<li>Christopher 'VdoP' Regali (retired)</li>"
+                "</ul>"
+
+                "<b>Contributors</b>"
+                "<ul>"
+                "<li>Luis Gustavo Spern Barreto &lt;gustavosbarreto@gmail.com&gt;</li>"
+                "<li>Bartosz Taczala &lt;w0lanto@gmail.com&gt;</li>"
+                "</ul>"
+                "<p>If you want to contribute, just visit <a href='http://www.razor-qt.org'>www.razor-qt.org</a>.</p>"
+                );
+}
+
+
+QString RazorAboutDLGPrivate::thanksText() const
+{
+    return  QString::fromUtf8(
+                "Special thanks to"
+                "<ul>"
+                "<li>Andy Fitzsimon (logo/icon)</li>"
+                "<li>Eugene Pivnev (QtDesktop)</li>"
+                "<li>Paul Gnther (for desktop backgrounds)</li>"
+                "<li>Manuel Meier (for ideas)</li>"
+                "<li>Alexander Zakher (for the name)</li>"
+                "<li>Alexey Nosov (for the A-MeGo theme)</li>"
+                "<li>and KDE (http://www.kde.org)</li>"
+                );
+}
+
+QString RazorAboutDLGPrivate::translationsText() const
+{
+    TranslatorsInfo translatorsInfo;
+    return QString("%1<p><ul>%2</ul>").arg(
+                tr("Razor-qt is translated into many languages thanks to the work of the translation teams all over the world."),
+                translatorsInfo.asHtml()
+                );
+}
+
+#define TechnicalTextRow QString("<tr><td width='1%'><div class=techInfoKey>%1</div></td><td>%2</td></tr>")
+
+QString RazorAboutDLGPrivate::technicalText() const
+{
+    // technical info
+#ifdef DEBUG
+   QString debug(tr("Yes"));
+#else
+   QString debug(tr("No"));
+#endif
+   XdgDirs xdgDirs;
+   QString tech;
+
+   tech += tr("<b>Razor Desktop Toolbox - Technical Info</b>");
+   tech += "<table width='100%'>";
+   tech += TechnicalTextRow.arg(tr("Version"),              RAZOR_VERSION);
+   tech += TechnicalTextRow.arg(tr("Qt"),                   qVersion());
+   tech += TechnicalTextRow.arg(tr("Debug Build"),          debug);
+   tech += TechnicalTextRow.arg(tr("System Configuration"), RAZOR_ETC_XDG_DIRECTORY);
+   tech += TechnicalTextRow.arg(tr("Share Directory"),      SHARE_DIR);
+   tech += TechnicalTextRow.arg(tr("Translations"),         TRANSLATIONS_DIR);
+   tech += "</table>";
+
+   tech += "<p>";
+
+   tech += tr("<b>User Directories</b>");
+   tech += "<table width='100%'>";
+   tech += TechnicalTextRow.arg(tr("Xdg Data Home"),        xdgDirs.dataHome(false));
+   tech += TechnicalTextRow.arg(tr("Xdg Config Home"),      xdgDirs.configHome(false));
+   tech += TechnicalTextRow.arg(tr("Xdg Data Dirs"),        xdgDirs.dataDirs().join(":"));
+   tech += TechnicalTextRow.arg(tr("Xdg Cache Home"),       xdgDirs.cacheHome(false));
+   tech += TechnicalTextRow.arg(tr("Xdg Runtime Home"),     xdgDirs.runtimeDir());
+   tech += TechnicalTextRow.arg(tr("Xdg Autostart Dirs"),   xdgDirs.autostartDirs().join(":"));
+   tech += TechnicalTextRow.arg(tr("Xdg Autostart Home"),   xdgDirs.autostartHome(false));
+   tech += "</table>";
+
+   return tech;
 }
 
 
@@ -126,8 +203,6 @@ RazorAboutDLG::RazorAboutDLG()
 {
     d_ptr = new RazorAboutDLGPrivate();
 }
-
-
 
 
 #endif
