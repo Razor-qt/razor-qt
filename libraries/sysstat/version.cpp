74884b8abb63bc165bee96cfa4158120855644a6
Kuzma Shapran <kuzma.shapran@gmail.com>
2012-10-27 14:37:53 +1300

    Relicensing

--- libraries/sysstat/version.cpp
+++ libraries/sysstat/version.cpp
@@ -1,22 +1,32 @@
 /*************
 **
-** Project:      Qt-based interface to system statistics (libsysstat)
-** Author:       Copyright (C) 2009 - 2012 Kuzma Shapran <Kuzma.Shapran@gmail.com>
-** License:      LGPLv3
-**
 **  This file is part of SysStat library.
+**
+**  SysStat is a Qt-based interface to system statistics
+**
+**  Authors:
+**       Copyright (c) 2009 - 2012 Kuzma Shapran <Kuzma.Shapran@gmail.com>
+**
+**  Licenses:  LGPL2+, LGPLv3
 **
 **  This library is free software; you can redistribute it and/or modify it
 **  under the terms of the GNU Lesser General Public License as published by
-**  the Free Software Foundation; either version 3 of the License, or any
-**  later version. This work is distributed in the hope that it will be
-**  useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  the Free Software Foundation;
+**  when it is used as a part of Razor-qt (http://razor-qt.org):
+**   either version 2.1 of the License, or (at your option) any later version
+**  otherwise:
+**   either version 3 of the License, or (at your option) any later version.
+**
+**  This work is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
 **  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 **  See the GNU Lesser General Public License for more details.
 **
 **  You should have received a copy of the GNU Lesser General Public License
 **  along with the library.
-**  If not, see <http://www.gnu.org/licenses/lgpl.html>.
+**  If not, write to the Free Software Foundation, Inc.,
+**  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA .
+**  Or see <http://www.gnu.org/licenses/lgpl.html>.
 **
 *************/
 
