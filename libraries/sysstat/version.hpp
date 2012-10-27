8c32f68fd785aaad16a51e4dc53fb5c1b5836257
Kuzma Shapran <kuzma.shapran@gmail.com>
2012-10-28 11:36:07 +1300

    Relicensing again

--- libraries/sysstat/version.hpp
+++ libraries/sysstat/version.hpp
@@ -1,34 +1,27 @@
-/*************
-**
-**  This file is part of SysStat library.
+/* BEGIN_COMMON_COPYRIGHT_HEADER
+ * (c)LGPL2+
 **
 **  SysStat is a Qt-based interface to system statistics
 **
 **  Authors:
 **       Copyright (c) 2009 - 2012 Kuzma Shapran <Kuzma.Shapran@gmail.com>
 **
-**  Licenses:  LGPL2+, LGPLv3
+**  This library is free software; you can redistribute it and/or
+**  modify it under the terms of the GNU Lesser General Public
+**  License as published by the Free Software Foundation; either
+**  version 2.1 of the License, or (at your option) any later version.
 **
-**  This library is free software; you can redistribute it and/or modify it
-**  under the terms of the GNU Lesser General Public License as published by
-**  the Free Software Foundation;
-**  when it is used as a part of Razor-qt (http://razor-qt.org):
-**   either version 2.1 of the License, or (at your option) any later version
-**  otherwise:
-**   either version 3 of the License, or (at your option) any later version.
-**
-**  This work is distributed in the hope that it will be useful,
+**  This library is distributed in the hope that it will be useful,
 **  but WITHOUT ANY WARRANTY; without even the implied warranty of
-**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-**  See the GNU Lesser General Public License for more details.
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+**  Lesser General Public License for more details.
 **
-**  You should have received a copy of the GNU Lesser General Public License
-**  along with the library.
-**  If not, write to the Free Software Foundation, Inc.,
-**  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA .
-**  Or see <http://www.gnu.org/licenses/lgpl.html>.
+**  You should have received a copy of the GNU Lesser General Public
+**  License along with this library;
+**  if not, write to the Free Software Foundation, Inc.,
+**  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **
-*************/
+** END_COMMON_COPYRIGHT_HEADER */
 
 
 #ifndef LIBSYSSTAT__VERSION__INCLUDED
