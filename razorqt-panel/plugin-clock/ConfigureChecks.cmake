cd2a62d44038a744c5bf10d589218ebbd14cda40
Luís Pereira <luis.artur.pereira@gmail.com>
2012-11-12 14:46:12 -0800

    plugin-clock: Check if _NL_TIME_WEEK_1STDAY exists
    Signed-off-by: Luís Pereira <luis.artur.pereira@gmail.com>

--- razorqt-panel/plugin-clock/ConfigureChecks.cmake
+++ razorqt-panel/plugin-clock/ConfigureChecks.cmake
@@ -7,6 +7,8 @@ check_cxx_source_compiles("
     int main()
     {
         const char *r = nl_langinfo(_NL_TIME_FIRST_WEEKDAY);
+        const char *s = nl_langinfo(_NL_TIME_WEEK_1STDAY);
+
         return 0;
     }
 " HAVE__NL_TIME_FIRST_WEEKDAY)
