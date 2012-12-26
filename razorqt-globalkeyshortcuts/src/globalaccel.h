25af9ad6de8b296156ed8367eb1dfdc71885d2a9
Alec Moskvin <alecm@gmx.com>
2012-12-26 17:50:50 -0500

    globalkeyshortucts: fix segfault and other bugs in daemon
    Segmentation fault happened if a deleted shortcut was triggered. Also
    shortcuts were never unregistered and objects were never freed...
    (cherry picked from commit 697e6e494e8cbd9547e50e797153b778ee47a128)

--- razorqt-globalkeyshortcuts/src/globalaccel.h
+++ razorqt-globalkeyshortcuts/src/globalaccel.h
@@ -1,37 +1,32 @@
 /* BEGIN_COMMON_COPYRIGHT_HEADER
- * (c)LGPL2+
- *
- * Razor - a lightweight, Qt based, desktop toolset
- * http://razor-qt.org
- *
- * Copyright: 2012 Razor team
- * Authors:
- *   Aaron Lewis <the.warl0ck.1989@gmail.com>
- *
- * This program or library is free software; you can redistribute it
- * and/or modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * (c)LGPL2+
+ *
+ * Razor - a lightweight, Qt based, desktop toolset
+ * http://razor-qt.org
+ *
+ * Copyright: 2012 Razor team
+ * Authors:
+ *   Aaron Lewis <the.warl0ck.1989@gmail.com>
+ *
+ * This program or library is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
 
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
- *
- * END_COMMON_COPYRIGHT_HEADER */
-#include <QProcess>
-#include <QSettings>
-#include <QMap>
-#include <QKeySequence>
-#include <QObject>
-#include <QDebug>
-#include <QDBusMessage>
-#include <QDBusConnection>
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+ * END_COMMON_COPYRIGHT_HEADER */
+
+#include <QtGui/QKeySequence>
+#include <QtDBus/QDBusMessage>
 
 #include <razorqt/razorsettings.h>
 #include <razorqxt/qxtglobalshortcut.h>
@@ -40,17 +35,24 @@ class AbstractShortcut : public QObject
 {
     Q_OBJECT
     public:
-       AbstractShortcut (QObject *parent = 0) {}
+        AbstractShortcut(const QKeySequence& sequence, QObject* parent = 0);
+        ~AbstractShortcut() {}
 
-       /* Execute the command */
-       virtual bool exec() = 0;
+    private slots:
+        /* Execute the command */
+        virtual bool exec() = 0;
+
+    private:
+        QxtGlobalShortcut mShortcut;
 };
 
 class CommandShortcut : public AbstractShortcut
 {
     Q_OBJECT
     public:
-        CommandShortcut(const QString & cmd, QObject *parent = 0);
+        CommandShortcut(const QKeySequence& sequence, const QString& cmd, QObject* parent = 0);
+
+    private slots:
         bool exec();
 
     private:
@@ -62,23 +64,19 @@ class DBusShortcut : public AbstractShortcut
 {
     Q_OBJECT
     public:
-        DBusShortcut( const QString & dest, 
+        DBusShortcut(const QKeySequence& sequence,
+                const QString & dest,
                 const QString & path,
                 const QString & interface,
                 const QString & method,
                 const QList<QVariant> & parameters = QList<QVariant>(),
                 QObject *parent = 0);
         
+    private slots:
         bool exec();   
 
     private:
-        QString m_dest,           /* DBUS destination */
-                m_path,           /* DBUS path */
-                m_interface,      /* Call interface */
-                m_method;         /* Call method */
-
-        QList<QVariant> m_params; /* Call parameters */
-
+        QDBusMessage mMessage;
 };
 
 class GlobalAccel: public QObject
@@ -89,13 +87,10 @@ public:
     ~GlobalAccel();
     GlobalAccel(QObject *parent = 0);
 
-    void bindDefault();
-
 private:
     RazorSettings *m_shortcutSettings;
-    QMap<QKeySequence, AbstractShortcut*> mapping;
+    QList<AbstractShortcut*> mMapping;
 
 private slots:
-    void launchApp();
     void rebindAll();
 };
