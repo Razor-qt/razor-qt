b9abb09b11519f10da427348a175838270db871e
Alexander Sokolov <sokoloff.a@gmail.com>
2013-01-12 15:15:55 +0400

    Compatability with liblightdm-qt-1-dev
    Conflicts:
    
    	razorqt-lightdm-greeter/loginform.cpp

--- razorqt-lightdm-greeter/loginform.cpp
+++ razorqt-lightdm-greeter/loginform.cpp
@@ -73,7 +73,11 @@ LoginForm::LoginForm(QWidget *parent) : QWidget(parent), ui(new Ui::LoginForm)
     for (int row = 0; row < ui->sessionCombo->model()->rowCount(); row++)
     {
         QModelIndex index = ui->sessionCombo->model()->index(row, 0);
+#ifdef USING_LIGHTDM_QT_1
+        if (QString("razor") ==  ui->sessionCombo->model()->data(index, QLightDM::SessionsModel::IdRole).toString())
+#else
         if (QString("razor") ==  ui->sessionCombo->model()->data(index, QLightDM::SessionsModel::KeyRole).toString())
+#endif
         {
             ui->sessionCombo->setCurrentIndex(row);
             break;
